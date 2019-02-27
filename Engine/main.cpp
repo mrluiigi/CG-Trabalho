fc9391d85103aee4ee1c4e6a2b399dddc6bc151a
Luís Correia <luisbcorreia98@gmail.com>
2019-02-27 17:27:31 +0000

    Adicionado esqueleto engine

---
+++ Engine/main.cpp
@@ -0,0 +1,86 @@
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#include <math.h>
+
+
+
+void changeSize(int w, int h) {
+
+    // Prevent a divide by zero, when window is too short
+    // (you cant make a window with zero width).
+    if(h == 0)
+        h = 1;
+
+    // compute window's aspect ratio
+    float ratio = w * 1.0 / h;
+
+    // Set the projection matrix as current
+    glMatrixMode(GL_PROJECTION);
+    // Load Identity Matrix
+    glLoadIdentity();
+
+    // Set the viewport to be the entire window
+    glViewport(0, 0, w, h);
+
+    // Set perspective
+    gluPerspective(45.0f ,ratio, 1.0f ,1000.0f);
+
+    // return to the model view matrix mode
+    glMatrixMode(GL_MODELVIEW);
+}
+
+
+void renderScene(void) {
+
+    // clear buffers
+
+    // set the camera
+    glLoadIdentity();
+    gluLookAt(5.0,5.0,5.0,
+              0.0,0.0,0.0,
+              0.0f,1.0f,0.0f);
+
+
+    // put the geometric transformations here
+
+
+    // put drawing instructions here
+
+
+    glEnd();
+    // End of frame
+    glutSwapBuffers();
+}
+
+
+int main(int argc, char **argv) {
+
+// init GLUT and the window
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DEPTH|GLUT_DOUBLE|GLUT_RGBA);
+    glutInitWindowPosition(100,100);
+    glutInitWindowSize(1920,1080);
+    glutCreateWindow("Projeto_CG");
+    glClearColor(1.0,1.0,1.0,1.0);
+
+// Required callback registry
+    glutDisplayFunc(renderScene);
+    glutReshapeFunc(changeSize);
+
+
+// put here the registration of the keyboard callbacks
+
+
+//  OpenGL settings
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_CULL_FACE);
+
+// enter GLUT's main cycle
+    glutMainLoop();
+
+    return 1;
+}
