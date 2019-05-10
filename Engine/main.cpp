2743e9345b329b7ab09de0b651c0bed5010dbbac
a81317 <a81317@alunos.uminho.pt>
2019-05-10 21:29:38 +0100

    Alterado funcionamento dos buffers

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -138,7 +138,7 @@ void drawGroup(Group group) {
         glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
         glBindBuffer(GL_ARRAY_BUFFER,normals[id]);
-        glNormalPointer(GL_FLOAT,0,0);
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
         glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indexes[id]);
 
@@ -186,8 +186,10 @@ void loadBuffers() {
         glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(int) * m.numberOfIndices, m.indicesBuffer, GL_STATIC_DRAW);
 
         //Preencher o buffer de normais
+        glEnableVertexAttribArray(2);
         glBindBuffer(GL_ARRAY_BUFFER,normals[i]);
-        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * m.numberOfNormals, m.normalsBuffer, GL_STATIC_DRAW);
+        glVertexAttribPointer(2, 3, GL_FLOAT, GL_FALSE, 0, 0);
+        glBufferData(GL_ARRAY_BUFFER, sizeof(float) * m.numberOfNormals * 3, m.normalsBuffer, GL_STATIC_DRAW);
     }
 }
 
@@ -207,7 +209,7 @@ void renderScene(void) {
             0.0,0.0,0.0,
             0.0f,1.0f,0.0f);
 
-    GLfloat dir[4] = {0.0, 1.0 ,0.0, 0.0};
+    GLfloat dir[4] = {0.0, 1.0 ,1.0, 0.0};
     glLightfv(GL_LIGHT0, GL_POSITION, dir);
 
 
@@ -326,7 +328,7 @@ int main(int argc, char **argv) {
 
         //OpenGL settings
         glEnable(GL_DEPTH_TEST);
-        glEnable(GL_CULL_FACE);
+       // glEnable(GL_CULL_FACE);
         glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
         //Light Settings
